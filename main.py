389b7a1c9a58df863f54c2279296bc5b457ee9ae
송승혁 <tmdgur328@naver.com>
2025-05-17 15:58:54 +0900

    주석 생성

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ player_index = 0
 enemy_index = 0 
 distanceThreshold = 80
 
-# 사용자 화면 크기에 맞게 다시 저장해야함 (개인마다 크기가  다르기 때문)
+# 사용자 화면 크기에 맞게 설정 (개인마다 크기가 다르기 때문)
 player = [
     Object('images/T_REX.png'),
     Object('images/T_REX_b.png')
@@ -43,7 +43,7 @@ while True:
     img = grabScreen()
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     if player[0].match(img):
-        # 이것도 사용자마다 크기가 달라서 개인적으로 설정해야함
+        # 사용자 화면 크기에 맞게 설정
         topleft_x = int(player[0].location[0][0] - player[0].width)
         topleft_y = int(player[0].location[0][1] - 3 *  player[0].height)
         bottomRight_x = int(player[0].location[1][0] + 14 * player[0].width)
@@ -60,7 +60,7 @@ while True:
     img_0 = grabScreen(bbox=(*screenStart, *screenEnd))
     img = cv2.cvtColor(img_0, cv2.COLOR_BGR2GRAY)
  
-    #  낮/밤 테마 판별
+    #  낮/밤 배경 판별
     if player[0].match(img):
         player_index = 0 
         enemy_index = 0
@@ -78,8 +78,6 @@ while True:
         pyautogui.press('space')
     
     # 거리 기준 조절 (속도 증가에 따라 점프 거리 앞당기기)
-    # prevTime은 가장 최근에 이 로직이 실행된 시점을 저장
-	# > 1이라는 조건은 "마지막 업데이트 이후 1초 이상이 지났는가?"를 판단하는 조건
     if time.time() - prevTime > 1:
         if time.time() - startTime < 180 and player[player_index].location:
             distanceThreshold += speedRate
@@ -90,14 +88,18 @@ while True:
     if player[player_index].location: 
         cv2.rectangle(img_0, player[player_index].location[0], player[player_index].location[1], (255, 0, 0), 2)
 
+    # 장애물 감지 및 충돌 회피 로직
     for enemy in enemies[enemy_index]:
+        # 현재 화면에서 장애물 매칭 시도
         if enemy.match(img):
             cv2.rectangle(img_0, enemy.location[0], enemy.location[1], (0, 0, 255), 2)
 
+            # 플레이어 위치가 감지된 경우에만 거리 계산
             if player[player_index].location:
                 horizontalDistance = enemy.location[0][0] - player[player_index].location[1][0]
                 verticalDistance = player[player_index].location[0][1] - enemy.location[1][1]
 
+                # 충돌 위험이 있는 경우 (수평 거리가 임계값보다 작고, 수직 거리가 2픽셀 미만일 때)
                 if horizontalDistance < distanceThreshold and verticalDistance < 2:
                     pyautogui.press('space')
                     break                     
